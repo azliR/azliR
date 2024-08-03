2cecb738fd26b8c60af4215c628acddf9a2b1029
Rizal Hadiyansah <rizalhadiyansah@gmail.com>
2024-08-03 21:39:09 +0700

    Built web
    Branch: master
    Commit: 08023661b54151f40088b030618dea668f4cf2e1 (dirty)
    
    package:peanut 5.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2927543067"
+    serviceWorkerVersion: "3014194838"
   }
 });
